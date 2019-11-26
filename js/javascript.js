2cf054349ed7dfadda72567902d3dec9bd2fccaf
jonkingseestheworld <johnnykinglau@gmail.com>
2019-11-26 15:16:15 +0000

    updated 26Nov19

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+//refresh page on browser resize
+
+/*if (document.) */
+
+if (window.screen.width > '1000') {
+document.getElementById("brainstill").innerHTML = "<iframe src='jkl_brain.html' width='85%'' height='330px' bgcolor='red' align='center'>";
+} 
+
+
+
+/* <iframe src="jkl_brain.html" width=90% height=360px align="center" style="padding:0; border:0; border: 0px; margin: -35px 0px 0px; padding: 0px;"  scrolling="yes"; frameborder=0; > 
+        </iframe>
+
+         <!-- -ms-transform: scale(0.85);
+          -moz-transform: scale(0.85);
+          -o-transform: scale(0.85);
+          -webkit-transform: scale(0.85);
+          transform: scale(0.85); -->
+*/
+
+
+
+
+
+/*  $(window).bind('resize', function(e)
+{
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function()
+  {
+    location.reload(); // false to get page from cache 
+  }, 200);  
+  });    
+*/
